1da30285620b70625ffbeaa747743ec290f31065
Biscottone99 <112855868+Biscottone99@users.noreply.github.com>
2024-02-23 09:37:33 +0100

    Update hamnew.f90

--- hamnew.f90
+++ hamnew.f90
@@ -6,13 +6,13 @@ program hoppingtotal
   integer,allocatable::iwork(:)
   integer::nsiti,dim2, i, n, a, b, c, d, ab, cd, p, nf,j, k, l, perm, nao, ne
   integer::  temp, m, binarysearch, contasito
-  integer,allocatable:: vecconfig(:), occupazioni(:)
-  real*8,allocatable:: cord(:,:),rwork(:), w(:), dist(:,:,:), hop(:,:), nuclei(:,:), hop2(:,:)
-  complex*16,allocatable::ham(:,:),work(:), hamsoc(:,:),  soc_a(:,:,:), soc_b(:,:,:),soc_mono(:,:,:), pp(:,:,:)
-  real*8,allocatable:: umat(:,:), emat(:,:), corrint(:,:), correx(:,:), vmat(:,:,:),double(:), double2(:,:), dsite(:,:), ssite(:)
+  integer,allocatable:: vecconfig(:), occupazioni(:), NZ(:)
+  real*8,allocatable:: cord(:,:),rwork(:), w(:), dist(:,:,:), hop(:,:), nuclei(:,:), hop2(:,:), spintot(:), carica(:,:), q(:), u(:),esite(:)
+  complex*16,allocatable::ham(:,:),work(:), hamsoc(:,:),  soc_a(:,:,:), soc_b(:,:,:),soc_mono(:,:,:), pp(:,:,:), coup(:,:)
+  real*8,allocatable:: umat(:,:), emat(:,:), corrint(:,:), correx(:,:), vmat(:,:,:),double(:), double2(:,:), dsite(:,:), ssite(:), COUPLING(:)
   real*8:: Uc, t, PPP, me, gs, e, e0, pi, cl, radius
   logical:: bool, bool1, bool2, bool3
-  real*8::strenght,hbar,hbarev,echarge,emass,dpg, bubu, balu
+  real*8::strenght,hbar,hbarev,echarge,emass,dpg, bubu, balu, delta, gamma, gamma2
   complex*16::sy(2,2), spin(2,2,3),vec1(3),vec2(3), cp(3)
   complex*16::cplx,pf
   
@@ -20,17 +20,17 @@ program hoppingtotal
   nsiti=4
   ne=2
   
-  Uc=11.26d0
+  Uc=4d0
   dim2=28
   nf=dim2
-  t=2.5d0
+  t=2d0
   nso=nsiti*2
   me=9.1093837015d-31
   gs=2.00231930436256
   e=1.602176634d-19
   e0=8.8541878128d-12
   pi=dacos(-1.d0)
-  write(*,*) pi
+!  write(*,*) pi
   cl=299792458 
   cplx=cmplx(0.d0, 1.d0)
 
@@ -41,15 +41,37 @@ program hoppingtotal
   open(2,file='geom.dat')
   open(3,file='hamiltonian.dat')
   open(4,file='results.dat')
+  open(5,file='input.dat')
   open(6,file='check.dat')
   open(7,file='eig.dat')
-  
+  open(8,FILE='charges.dat')
+  open(9,file='work.dat')
 
-  allocate(vecconfig(dim2), cord(nsiti,3), nuclei(nsiti,3),ham(dim2,dim2), occupazioni(nsiti),dist(nsiti,nsiti,3), soc_a(3,nso,nso), soc_b(3,nso, nso), pp(3, nsiti,nsiti),hop(nsiti,nsiti),hop2(nso,nso),soc_mono(3,nso,nso),hamsoc(nso,nso))
+ 
+
+  allocate(vecconfig(dim2), cord(nsiti,3), nuclei(nsiti,3),ham(dim2,dim2), occupazioni(nsiti),dist(nsiti,nsiti,3), soc_a(3,nso,nso), soc_b(3,nso, nso), pp(3, nsiti,nsiti),hop(nsiti,nsiti),hop2(nso,nso),soc_mono(3,nso,nso),hamsoc(nso,nso), spintot(dim2), carica(nsiti,dim2), q(dim2), nz(nsiti), u(nsiti), esite(nsiti), coup(dim2,dim2), COUPLING(DIM2))
 
   do i=1,dim2
      read(1,*) vecconfig(i)
   enddo
+  u=uc
+  esite=0
+!!$  U(2)=uc
+!!$  U(3)=u(2)
+!!$  u(1)=-delta
+!!$  u(4)=+delta
+!!$
+!!$  esite(2)=0
+!!$  esite(3)=esite(2)
+!!$  esite(1)=gamma
+!!$  esite(4)=gamma2
+  
+!!$  do i=2,4
+!!$     u(i)=0
+!!$  enddo
+  nz=0
+  nz(1)=2
+  
 
   do i=1,nsiti
      read(2,*) cord(i,1), cord(i,2), cord(i,3)
@@ -91,11 +113,23 @@ program hoppingtotal
      PPP=0.d0
      do i=1,nsiti
         do p=1,nsiti
-           PPP=PPP+(14.397)/((28.794/(2*uc))**2+((cord(i,1)-cord(p,1))**2+(cord(i,2)-cord(p,2))**2+(cord(i,3)-cord(p,3))))**0.5*(1-occupazioni(i))*(1-occupazioni(p))
+           PPP=PPP+(14.397)/((28.794/(u(i)-u(p)))**2+((cord(i,1)-cord(p,1))**2+(cord(i,2)-cord(p,2))**2+(cord(i,3)-cord(p,3))))**0.5*(nz(i)-occupazioni(i))*(nz(p)-occupazioni(p))
         enddo
      enddo
      ham(n,n)=0.5*PPP
   enddo
+
+  do n=1,dim2
+     do i=0,nso-1,2
+        bool=btest(vecconfig(n),i)
+        bool1=btest(vecconfig(n),i+1)
+        if(bool)ham(n,n)=ham(n,n)+esite((i+2)/2)
+        if(bool1)ham(n,n)=ham(n,n)+esite((i+2)/2)
+        if(bool.and.bool1)ham(n,n)=ham(n,n)+u((i+2)/2)
+     enddo
+  enddo
+        
+        
   !=========================SUPPORTING=========================
   hop2=0
   do i=1,nso-2
@@ -124,8 +158,8 @@ program hoppingtotal
                     if(bool2)perm=perm+1
                  enddo
               endif
-              if(perm/2*2.eq.perm)ham(n,m)=hop2(i+1,i+3)
-              if(perm/2*2.ne.perm)ham(n,m)=-hop2(i+1,i+3)
+              if(perm/2*2.eq.perm)ham(n,m)=-hop2(i+1,i+3)
+              if(perm/2*2.ne.perm)ham(n,m)=+hop2(i+1,i+3)
 
            endif
         endif
@@ -313,11 +347,11 @@ program hoppingtotal
                     m=binarysearch(1,dim2,vecconfig,temp)
                               
                  if(2*perm/2.ne.perm)then
-                    ham(n,m)=ham(n,m)-pf*hamsoc(i+1,j+1)
+                    coup(n,m)=coup(n,m)-pf*hamsoc(i+1,j+1)
                  else
-                    ham(n,m)=ham(n,m)+pf*hamsoc(i+1,j+1)
+                    coup(n,m)=coup(n,m)+pf*hamsoc(i+1,j+1)
                  endif
-                 if(hamsoc(i+1,j+1).ne.0d0)write(77,*) n, m, i+1,j+1
+               !  if(hamsoc(i+1,j+1).ne.0d0)write(77,*) n, m, i+1,j+1
               endif
            enddo
         else
@@ -341,11 +375,11 @@ program hoppingtotal
                  m=binarysearch(1,dim2,vecconfig,temp)
 
                  if(2*perm/2.ne.perm)then
-                    ham(n,m)=ham(n,m)-pf*hamsoc(i+1,j+1)
+                    coup(n,m)=coup(n,m)-pf*hamsoc(i+1,j+1)
                  else
-                    ham(n,m)=ham(n,m)+pf*hamsoc(i+1,j+1)
+                    coup(n,m)=coup(n,m)+pf*hamsoc(i+1,j+1)
                  endif
-                 if(hamsoc(i+1,j+1).ne.0d0)write(77,*) n, m, i+1,j+1
+                ! if(hamsoc(i+1,j+1).ne.0d0)write(77,*) n, m, i+1,j+1
               endif
            enddo
         endif
@@ -398,8 +432,92 @@ write(4,*) 'IMAGINARY EIGENVECTORS'
 do i=1,nf
    write(4,'(<nf>(2x,f10.5))')(dimag(ham(i,j)),j=1,nf)
 enddo
+!=========================COUPLING=========================
+do i=1,dim2 !a
+   do j=1,dim2 !alfa 
+      do k=1,dim2 !beta
+         coupling(i)=coupling(i)+dconjg(ham(i,j))*ham(j,i)*dconjg(ham(k,i))*ham(i,k)*coup(j,k)
+      enddo
+   enddo
+enddo
+
+ write(77,*) 'IMAG COUP'
+  do i=1,dim2
+     write(77,'(<dim2>(2x,f10.5))') (dimag(coup(i,j)),j = 1,dim2)
+  enddo
+
+do i=1,dim2
+   write(77,*) coupling(i)
+enddo
 
 
+!=========================SPIN MEDIO=========================
+do n=1,dim2
+   do m=1,dim2
+      a=0
+      b=0
+      do i=0,nso-1,2
+         bool=btest(vecconfig(m),i)
+         bool1=btest(vecconfig(m),i+1)
+         if(bool)a=a+1
+         if(bool1)b=b+1
+      enddo
+      spintot(n)=spintot(n)+(a-b)*0.5*ham(m,n)**2
+   enddo
+enddo
+write(4,*) 'SPIN'
+do i=1,dim2
+   write(4,*)spintot(i)
+enddo
+
+!=========================CHARGES=========================
+
+do n=1,dim2
+   do i=0,nso-1,2
+      do m=1,dim2
+         bool=btest(vecconfig(m),i)
+         bool1=btest(vecconfig(m),i+1)
+         if(bool)then
+            a=1
+         else
+            a=0
+         endif
+         if(bool1)then
+            b=1
+         else
+            b=0
+         endif
+         carica((i+2)/2,n)=carica((i+2)/2,n)+(nz((i+2)/2)-a-b)*(zabs(ham(m,n)))**2
+      enddo
+   enddo
+enddo
+
+do n=1,dim2
+   do i=1,nsiti
+      q(n)=q(n)+carica(i,n)
+   enddo
+enddo
+
+
+write(4,*) 'CARICA'
+
+do i=1,nsiti
+   write(4,'(<dim2>(2x,f10.5))') (carica(i,j), j=1,dim2 )
+   
+enddo
+
+do i=1,nsiti
+   write(8,'(<2>(2x,f10.5))') (carica(i,j), j=1,2)
+enddo
+
+do i=1,2
+   do j=1,nsiti
+      write(9,*) carica(j,i)
+   enddo
+enddo
+
+
+!============================================================
 
 
 
